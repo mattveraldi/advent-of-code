4ca35f02914364b7bb26d279425317866051d65c
Matteo Veraldi <mattveraldi@gmail.com>
2023-12-09 17:00:21 +0100

    feat: add day 2 part 2

--- 2023/day-2/main.cpp
+++ 2023/day-2/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <regex>
 
-int main()
+int part_one()
 {
     std::fstream file("input.txt", std::fstream::in);
     const int red_limit = 12;
@@ -74,16 +74,91 @@ int main()
             token += character;
         }
     }
-    if (result != 2377)
+    file.close();
+    return result;
+}
+
+int part_two()
+{
+    std::fstream file("input.txt", std::fstream::in);
+    const int red_limit = 12;
+    const int green_limit = 13;
+    const int blue_limit = 14;
+    int red = 1;
+    int green = 1;
+    int blue = 1;
+    const std::regex id_reg("(\\d+)", std::regex_constants::ECMAScript);
+    const std::regex color_reg("(red|green|blue)");
+    char character;
+    int current_game_id = -1;
+    int result = 0;
+    std::string token = "";
+    while (file >> std::noskipws >> character)
     {
-        std::cerr << "That's not the right answer!"
-                  << "\n";
-        exit(1);
+        // am i parsing the game id?
+        if (character == ':')
+        {
+            std::smatch id_match;
+            std::regex_search(token, id_match, id_reg);
+            current_game_id = atoi(id_match[1].str().c_str());
+            token = "";
+        }
+        // is the end of the round, a cube set that I saw, or the end of the game?
+        else if (character == ',' || character == ';' || character == '\n')
+        {
+            // I treat end of the round and cube set that I saw the same.
+            // the concept is that you can't extract more cubes than the limit.
+            // evaluate number and color and update colors only if the game is still valid
+            std::smatch id_match;
+            std::smatch color_match;
+            std::regex_search(token, id_match, id_reg);
+            std::regex_search(token, color_match, color_reg);
+            const std::string color = color_match[1];
+            const int increment = atoi(id_match[1].str().c_str());
+            if (color == "red")
+                if (increment > red)
+                    red = increment;
+            if (color == "green")
+                if (increment > green)
+                    green = increment;
+            if (color == "blue")
+                if (increment > blue)
+                    blue = increment;
+
+            // am I approaching the end of this game?
+            if (character == '\n')
+            {
+                if (current_game_id == -1)
+                {
+                    std::cerr << "Parsing error: current_game_id is -1";
+                    exit(1);
+                }
+                const int power = red * green * blue;
+                result += power;
+                // next game
+                current_game_id = -1;
+                token = "";
+                red = 1;
+                green = 1;
+                blue = 1;
+            }
+
+            token = "";
+        }
+        // continue parsing...
+        else
+        {
+            token += character;
+        }
     }
-    else
-    {
-        std::cout << "That's the right answer!"
-                  << "\n";
-    }
-    return 0;
+    file.close();
+    return result;
+}
+
+int main()
+{
+    int one = part_one();
+    int two = part_two();
+    std::cout << "Part one: " << one << "\n"; // should be 2377
+    std::cout << "Part two: " << two << "\n"; // should be 71220
 }
